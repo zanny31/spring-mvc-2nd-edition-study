c1f2428556c5d3052d95f5519e0ea27392f7e944
zanny31 <jianb0331@naver.com>
2025-01-29 23:06:04 +0900

    [섹션2-연산] 연산 공부

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -94,6 +94,13 @@ public class BasicController {
 	 }
 	 
 	 
+	 @GetMapping("/operation")
+	 public String operation(Model model) {
+	    model.addAttribute("nullData", null);
+	    model.addAttribute("data", "Spring!");
+	 return "basic/operation";
+	 }
+	 
 	@Data
 	static class User {
 		private String username;
