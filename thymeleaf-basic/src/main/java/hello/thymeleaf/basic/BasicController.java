ec017eb32cd9b9d17b61c66327e6171f87d332e0
zanny31 <jianb0331@naver.com>
2025-01-29 22:09:18 +0900

    [섹션2-URL 링크] URL 링크 공부

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -22,22 +22,20 @@ import lombok.Setter;
 @Controller
 @RequestMapping("/basic")
 public class BasicController {
-	
+
 	@GetMapping("text-basic")
 	public String textBasic(Model model) {
 		model.addAttribute("data", "Hello Spring!");
-		
+
 		return "basic/text-basic";
 	}
 
-	
 	@GetMapping("text-unescaped")
 	public String textUnescaped(Model model) {
 		model.addAttribute("data", "Hello <b>Spring!</b>");
 		return "basic/text-unescaped";
 	}
-	
-	
+
 	@GetMapping("variable")
 	public String variable(Model model) {
 		User userA = new User("userA", 10);
@@ -46,55 +44,57 @@ public class BasicController {
 		List<User> list = new ArrayList<>();
 		list.add(userA);
 		list.add(userB);
-		
-		Map<String,User> map = new HashMap<>();
+
+		Map<String, User> map = new HashMap<>();
 		map.put("userA", userA);
-		map.put("userB", userB); 
-		
+		map.put("userB", userB);
+
 		model.addAttribute("user", userA);
-		model.addAttribute("users",list);
+		model.addAttribute("users", list);
 		model.addAttribute("userMap", map);
-		
-		
+
 		return "basic/variable";
 	}
-	
-	
-	
-	 @GetMapping("/basic-objects")
-	 public String basicObjects(Model model, HttpServletRequest request, 
-	HttpServletResponse response, HttpSession session) {
-	    session.setAttribute("sessionData", "Hello Session");
-	    model.addAttribute("request", request);
-	    model.addAttribute("response", response);
-	    model.addAttribute("servletContext", request.getServletContext());
-	 return "basic/basic-objects";
-	 }
-	 
-	 @Component("helloBean")
-	 static class HelloBean {
-	 public String hello(String data) {
-	 return "Hello " + data;
-	    }
-	 }
-	
-	 @GetMapping("/date")
-	 public String date(Model model) {
-	    model.addAttribute("localDateTime", LocalDateTime.now());
-	 return "basic/date";
-	 }
-	
+
+	@GetMapping("/basic-objects")
+	public String basicObjects(Model model, HttpServletRequest request, HttpServletResponse response,
+			HttpSession session) {
+		session.setAttribute("sessionData", "Hello Session");
+		model.addAttribute("request", request);
+		model.addAttribute("response", response);
+		model.addAttribute("servletContext", request.getServletContext());
+		return "basic/basic-objects";
+	}
+
+	@Component("helloBean")
+	static class HelloBean {
+		public String hello(String data) {
+			return "Hello " + data;
+		}
+	}
+
+	@GetMapping("/date")
+	public String date(Model model) {
+		model.addAttribute("localDateTime", LocalDateTime.now());
+		return "basic/date";
+	}
+
+	@GetMapping("/link")
+	public String link(Model model) {
+		model.addAttribute("param1", "data1");
+		model.addAttribute("param2", "data2");
+		return "basic/link";
+	}
+
 	@Data
-	static class User{
+	static class User {
 		private String username;
 		private int age;
-		
+
 		public User(String username, int age) {
 			this.username = username;
 			this.age = age;
 		}
 	}
-	
-	
-	
+
 }
